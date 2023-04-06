85f83ee7b4786f21ac418ec906ff0d31e52b6f9a
Alejandro Velez <velez.alejandro4@gmail.com>
2023-04-06 18:29:17 -0400

    added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import './styles.css';
+import{Container,Navbar, NavbarBrand} from 'reactstrap';
+import FitFreakzLogo from './app/assets/img/FitFreakzLogo.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Navbar sticky = 'top' expand = 'md'>
+          <Container>
+            <NavbarBrand href = '/'>
+              <img src = {FitFreakzLogo} alt = 'Fitfreakz Logo'/>
+            </NavbarBrand>
+          </Container>
+        </Navbar>
       </header>
     </div>
   );
